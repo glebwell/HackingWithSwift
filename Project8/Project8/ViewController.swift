ca39ca3f661a75f526a2af65c31cea054e9569e8
glebwell <gleb.maltcev@yandex.ru>
2018-08-15 19:00:30 +0300

    project 8 was implemented until p. 275

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Gleb on 15.08.2018.
+//  Copyright © 2018 Gleb Maltsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var cluesLabel: UILabel!
+  @IBOutlet weak var answersLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var currentAnswer: UITextField!
+
+  @IBAction func submitTapped(_ sender: UIButton) {
+  }
+
+  @IBAction func clearTapped(_ sender: UIButton) {
+    
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
