249f028f168a2f9bd91dd5474725043cc944835e
glebwell <gleb.maltcev@yandex.ru>
2018-08-17 22:43:33 +0300

    project 10 was implemented

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Gleb on 17.08.2018.
+//  Copyright © 2018 Gleb Maltsev. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var name: UILabel!
+}
